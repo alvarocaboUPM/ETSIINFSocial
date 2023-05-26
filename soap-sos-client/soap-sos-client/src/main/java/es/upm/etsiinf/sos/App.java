c2290271be697cb4647b85e7ae2210e35b51d234
Álvaro Cabo <113105033+alvarocaboUPM@users.noreply.github.com>
2023-05-26 12:17:31 +0200

    Created auth module (#1)
    Co-authored-by: alvarocabo <alvarocaboac2@gmail.com>

---
+++ soap-sos-client/soap-sos-client/src/main/java/es/upm/etsiinf/sos/App.java
@@ -0,0 +1,13 @@
+package es.upm.etsiinf.sos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
