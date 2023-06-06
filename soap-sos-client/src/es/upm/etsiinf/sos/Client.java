ecc5f5604484ee93e8b4b5bfda2b35869dc2e69f
alvarocabo <alvarocaboac2@gmail.com>
2023-06-06 13:21:58 +0000

    Testing secuencial

--- soap-sos-client/src/es/upm/etsiinf/sos/Client.java
+++ soap-sos-client/src/es/upm/etsiinf/sos/Client.java
@@ -1,59 +1,56 @@
 package es.upm.etsiinf.sos;
 
 import es.upm.etsiinf.sos.ws.*;
-import es.upm.etsiinf.sos.ws.ETSIINFSocialStub.Response;
 import java.rmi.RemoteException;
-import java.util.concurrent.BlockingQueue;
 
 public class Client {
-	static Response response;
-	static ETSIINFSocialStub[] clientes;
-	static Ops[] operaciones;
 
-	static int NumTest = 10;
-	static int RanClk = 0;
-	static int RanOp = 0;
-	static int nCliente;
-	
-	static BlockingQueue<String> messageQueue;
+	static final int NumTest = 11;
+	static ETSIINFSocialStub[] clientes;
 
 	public static void main(String[] args) throws RemoteException {
 
 		clientes = new ETSIINFSocialStub[NumTest];
-		operaciones = new Ops[NumTest];
 		for (int i = 0; i < NumTest; i++) {
 			clientes[i] = new ETSIINFSocialStub();
 			// mantenemos las conexiones
 			clientes[i]._getServiceClient().engageModule("addressing");
 			clientes[i]._getServiceClient().getOptions().setManageSession(true);
-			
 		}
 
-		// --------------------------------------
-		// | Cliente 1 |
-		// --------------------------------------
-		operaciones[0] = new Ops(clientes[0]);
-		operaciones[0].login("admin", "admin");
-		
-		operaciones[0].addUser("dummyass");
+		Thread[] threads = new Thread[NumTest];
+		for (int i = 0; i < NumTest; i++) {
+			threads[i] = new ClientThread(i);
+		}
+
+		//Diccionario que mapea ID con tarea
+		Tests.fillCaseMap();
+
+		// Iniciar los hilos secuencialmente (sin login)
+		for (int i = 1; i < NumTest; i++) {
+			threads[i].run();
+		}
+
 
 	}
 
-	
-	// private static class ClientThread extends Thread {
-	// 	int id;
+	private static class ClientThread extends Thread {
+		int id;
+		Ops op;
 
-	// 	ClientThread(int id) {
-	// 		this.id = id;
-	// 	}
+		ClientThread(int id) {
+			this.id = id;
+			op = new Ops(clientes[id]);
+		}
 
-	// 	@Override
-	// 	public void run() {
-	// 		try {
-	// 			realizarOperacionesAleatorias(id);
-	// 		} catch (RemoteException e) {
-	// 			e.printStackTrace();
-	// 		}
-	// 	}
-	// }
+		@Override
+		public void run() {
+			try {
+				op.login("admin", "admin");
+				Tests.realizarOperacionesAleatorias(id, op);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- soap-sos-client/src/es/upm/etsiinf/sos/Tests.java
+++ soap-sos-client/src/es/upm/etsiinf/sos/Tests.java
@@ -1,168 +1,170 @@
 package es.upm.etsiinf.sos;
 
 import java.rmi.RemoteException;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Tests {
-    static boolean rootAccess;
-    static Random random = new Random();
+	static boolean rootAccess;
+	static Random random = new Random();
+	static HashMap<Integer, String> caseMap = new HashMap<>();
+	
+	static String[] users = {
+			"Alejandro",
+			"Sofía",
+			"Javier",
+			"María",
+			"Daniel",
+			"Laura",
+			"Adrián",
+			"Lucía",
+			"Pablo",
+			"Andrea",
+			"Carlos",
+			"Carmen",
+			"Diego",
+			"Natalia",
+			"Fernando",
+			"Elena",
+			"Rubén",
+			"Marta",
+			"Sergio"
+	};
 
-    static String[] users = {
-            "admin",
-            "Alejandro",
-            "Sofía",
-            "Javier",
-            "María",
-            "Daniel",
-            "Laura",
-            "Adrián",
-            "Lucía",
-            "Pablo",
-            "Andrea",
-            "Carlos",
-            "Carmen",
-            "Diego",
-            "Natalia",
-            "Fernando",
-            "Elena",
-            "Rubén",
-            "Marta",
-            "Sergio"
-    };
+	static String[] pass = {
+			"Contraseña1",
+			"Pass123",
+			"SecurePwd",
+			"Secret@123",
+			"P@ssw0rd",
+			"MyPassword",
+			"StrongPwd",
+			"12345678",
+			"Qwerty123",
+			"Welcome1",
+			"Pa$$w0rd",
+			"Password123!",
+			"SecurePass",
+			"123456",
+			"Abcd1234",
+			"Passw0rd",
+			"Password!",
+			"SecretPassword",
+			"Admin123"
+	};
 
-    static String[] pass = {
-            "admin",
-            "Contraseña1",
-            "Pass123",
-            "SecurePwd",
-            "Secret@123",
-            "P@ssw0rd",
-            "MyPassword",
-            "StrongPwd",
-            "12345678",
-            "Qwerty123",
-            "Welcome1",
-            "Pa$$w0rd",
-            "Password123!",
-            "SecurePass",
-            "123456",
-            "Abcd1234",
-            "Passw0rd",
-            "Password!",
-            "SecretPassword",
-            "Admin123"
-    };
+	static String[] states = {
+			"¡Feliz de estar aquí!",
+			"Disfrutando de un día soleado.",
+			"Deseando que llegue el fin de semana.",
+			"Emocionado por el concierto de esta noche.",
+			"Compartiendo una foto de mis vacaciones.",
+			"Reflexionando sobre la vida.",
+			"Probando una nueva receta.",
+			"Viendo mi serie favorita.",
+			"Celebrando un hito personal.",
+			"De vuelta al trabajo después de las vacaciones.",
+			"Apoyando a mi equipo en el partido de hoy.",
+			"Compartiendo una nueva canción que me encanta.",
+			"Planeando una escapada de fin de semana.",
+			"Haciendo ejercicio en el gimnasio.",
+			"Conectando con viejos amigos.",
+			"Explorando nuevos lugares en la ciudad.",
+			"Compartiendo una cita inspiradora.",
+			"Leyendo un libro fascinante.",
+			"Disfrutando de una tarde relajante en casa.",
+			"Agradecido por todo lo bueno en mi vida."
+	};
 
-    static String[] states = {
-            "¡Feliz de estar aquí!",
-            "Disfrutando de un día soleado.",
-            "Deseando que llegue el fin de semana.",
-            "Emocionado por el concierto de esta noche.",
-            "Compartiendo una foto de mis vacaciones.",
-            "Reflexionando sobre la vida.",
-            "Probando una nueva receta.",
-            "Viendo mi serie favorita.",
-            "Celebrando un hito personal.",
-            "De vuelta al trabajo después de las vacaciones.",
-            "Apoyando a mi equipo en el partido de hoy.",
-            "Compartiendo una nueva canción que me encanta.",
-            "Planeando una escapada de fin de semana.",
-            "Haciendo ejercicio en el gimnasio.",
-            "Conectando con viejos amigos.",
-            "Explorando nuevos lugares en la ciudad.",
-            "Compartiendo una cita inspiradora.",
-            "Leyendo un libro fascinante.",
-            "Disfrutando de una tarde relajante en casa.",
-            "Agradecido por todo lo bueno en mi vida."
-    };
+	public static void realizarOperacionesAleatorias(int thdID, Ops op) throws RemoteException {
+		rootAccess = false;
+		System.out.println(" 	--------------------------------------");
+		System.out.printf("		running: '%s'			  \n", caseMap.get(thdID));
+		System.out.println(" 	--------------------------------------");
+		realizarOperacion(thdID, op);
+	}
 
-    public static void realizarOperacionesAleatorias(int thdID) throws RemoteException {
-        rootAccess = false;
-        Client.operaciones[thdID] = new Ops(Client.clientes[thdID]);
-        System.out.println(" 	--------------------------------------");
-        System.out.printf("			cliente %d			  \n", thdID);
-        System.out.println(" 	--------------------------------------");
-        realizarOperacion(thdID + 1);
-    }
-
-    private static void realizarOperacion(int tipoOperacion) throws RemoteException {
+	private static void realizarOperacion(int tipoOperacion, Ops op) throws RemoteException {
 		String tmpUser, tmpPass;
 
 		switch (tipoOperacion) {
-
 			case 0:
-				System.out.println("Loging out");
-				Client.operaciones[tipoOperacion].logout();
-				break;
-			case 1:
 				System.out.println("Loging in");
 				tmpUser = generarNombre();
 				tmpPass = generarContraseña();
 				rootAccess = tmpUser.equals("admin") && tmpPass.equals("admin");
-				Client.operaciones[tipoOperacion].login("admin", "admin");
+				op.login("admin", "admin");
+				rootAccess = true;
 				break;
-			case 2: {
+			case 1:
 				System.out.println("Adding user");
 				String addingUser = generarNombre();
 				if (rootAccess) {
-					Client.operaciones[tipoOperacion].addUser(addingUser);
+					op.addUser(addingUser);
 					break;
-				} else {
-
-					System.out.println("Accion reservada para el usuario Administrador");
-					System.out.print("¿Iniciar Sesion Como Administrador? (yes/no): ");
-					boolean randomDecision = random.nextInt(2) == 1;
-					System.out.print(randomDecision ? "yes\n" : "no\n");
-
-					if (randomDecision) {
-						System.out.println("Introduce las credenciales del Administrador:\n");
-						tmpUser = generarNombre();
-						tmpPass = generarContraseña();
-						System.out.println("User: " + tmpUser);
-						System.out.println("Password: " + tmpPass);
-						if (tmpUser.equals("admin") && tmpPass.equals("admin")) {
-							Client.operaciones[tipoOperacion].login(tmpUser, tmpPass);
-							Client.operaciones[tipoOperacion].addUser(addingUser);
-						} else
-							System.out.println("Error al Iniciar sesion como Administrador");
-					} else {
-						System.out.println("Cancelando operacion...");
-					}
 				}
 
-			}
-			case 3:
-				System.out.println("Removing user");
-				Client.operaciones[tipoOperacion].removeUser(generarNombre());
+				System.out.println("Accion reservada para el usuario Administrador");
+				System.out.print("¿Iniciar Sesion Como Administrador? (yes/no): ");
+
+				boolean randomDecision = random.nextInt(2) == 1;
+				System.out.print(randomDecision ? "yes\n" : "no\n");
+				if (!randomDecision) {
+					System.out.println("Cancelando operacion...");
+					break;
+				}
+
+				System.out.println("Introduce las credenciales del Administrador:\n");
+				tmpUser = generarNombre();
+				tmpPass = generarContraseña();
+				System.out.println("User: " + tmpUser);
+				System.out.println("Password: " + tmpPass);
+
+				if (tmpUser.equals("admin") && tmpPass.equals("admin")) {
+					op.login(tmpUser, tmpPass);
+					op.addUser(addingUser);
+				} else
+					System.out.println("Error al Iniciar sesion como Administrador");
+
 				break;
-			case 4:
+			case 2:
+				String username = generarNombre();
+				System.out.println("Removing user" + username);
+				op.removeUser(username);
+				break;
+			case 3:
 				System.out.println("Chaning user password");
 
-				Client.operaciones[tipoOperacion].changePassword(Client.operaciones[tipoOperacion].contraseña, generarContraseña());
+				op.changePassword(op.contraseña,
+						generarContraseña());
+				break;
+			case 4:
+				System.out.println("Adding a friend");
+				op.addFriend(generarNombre());
 				break;
 			case 5:
-				System.out.println("Adding a friend");
-				Client.operaciones[tipoOperacion].addFriend(generarNombre());
+				System.out.println("Getting friends list");
+				op.getMyFriends();
 				break;
 			case 6:
-				System.out.println("Getting friends list");
-				Client.operaciones[tipoOperacion].getMyFriends();
+				System.out.println("Removing a friend state");
+				op.removeFriend(generarNombre());
 				break;
 			case 7:
-				System.out.println("Removing a friend state");
-				Client.operaciones[tipoOperacion].removeFriend(generarNombre());
+				System.out.println("Publishing state");
+				op.publishState(generarEstado());
 				break;
 			case 8:
-				System.out.println("Publishing state");
-				Client.operaciones[tipoOperacion].publishState(generarEstado());
+				System.out.println("Getting states list");
+				op.getMyStates();
 				break;
 			case 9:
-				System.out.println("Getting states list");
-				Client.operaciones[tipoOperacion].getMyStates();
+				System.out.println("Getting my friends states list");
+				op.getMyFriendStates(op.usuario);
 				break;
 			case 10:
-				System.out.println("Getting my friends states list");
-				Client.operaciones[tipoOperacion].getMyFriendStates(Client.operaciones[tipoOperacion].usuario);
+				System.out.println("Loging out");
+				op.logout();
 				break;
 			default:
 				System.out.println("operacion Invalida");
@@ -181,5 +183,22 @@ public class Tests {
 
 	private static String generarEstado() {
 		return states[random.nextInt(states.length)];
+	}
+
+	public static void fillCaseMap(){
+		caseMap = new HashMap<Integer, String>();
+
+		caseMap.put(0, "Loging in");
+        caseMap.put(1, "Adding user");
+        caseMap.put(2, "Removing user");
+        caseMap.put(3, "Chaning user password");
+        caseMap.put(4, "Adding a friend");
+        caseMap.put(5, "Getting friends list");
+        caseMap.put(6, "Removing a friend state");
+        caseMap.put(7, "Publishing state");
+        caseMap.put(8, "Getting states list");
+        caseMap.put(9, "Getting my friends states list");
+        caseMap.put(10, "Loging out");
+		
 	}
 }
